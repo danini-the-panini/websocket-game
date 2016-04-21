82e1a37ac7e86742b08b63177ba6fd06d1e8fba7
Daniel Smith <jellymann@gmail.com>
2016-04-22 00:33:18 +0200

    patch: use rollup command line instead of build script

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  entry: './server.js',
+  plugins: [
+    nodeResolve(),
+    commonjs({
+      exclude: './server.js'
+    })
+  ],
+  format: 'cjs'
+};
