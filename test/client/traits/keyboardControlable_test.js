676bc5dc6f2ff986f8cff9c57ed65f1bf2211600
Daniel Smith <jellymann@gmail.com>
2016-05-08 16:09:06 +0200

    add a keyboard controlable test

--- test/client/traits/keyboardControlable_test.js
+++ test/client/traits/keyboardControlable_test.js
@@ -1,9 +1,34 @@
-// import KeyboardControlable from "../../../src/traits/keyboardControlable";
-// import LawsOfMotion from "../../../src/traits/lawsOfMotion";
-// import Entity from "../../../src/entity";
+import THREE from "three";
+
+import "../../support/chai_three";
+
+import Game from "../../../src/game";
+import Entity from "../../../src/entity";
+import KeyboardControlable from "../../../src/traits/keyboardControlable";
+import Renderable from "../../../src/traits/renderable";
+import LawsOfMotion from "../../../src/traits/lawsOfMotion";
+
+const RIGHT = 37;
+const UP = 38;
+const LEFT = 39;
+const DOWN = 40;
+const SPACE = 32;
 
 describe("KeyboardControlable", () => {
   describe("#update", () => {
-    // TODO
+    it("moves the entity based on keyboard input", () => {
+      const game = new Game();
+      const entity = game.createEntity();
+      entity.addTrait(new Renderable(new THREE.Object3D()));
+      entity.addTrait(new LawsOfMotion());
+      entity.addTrait(new KeyboardControlable());
+
+      const keys = {};
+      keys[UP] = true;
+
+      entity.update({ delta: 1, keys: keys });
+
+      expect(entity.traits.LawsOfMotion.velocity).to.be.vector3(0, 0.0003, 0);
+    });
   });
 });
