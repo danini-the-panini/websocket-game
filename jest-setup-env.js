ef826e4adb25a59d882668021bc45cfedcd99106
Daniel Smith <jellymann@gmail.com>
2016-08-14 12:22:24 +0200

    fix: junit test results not working properly

--- jest-setup-env.js
+++ jest-setup-env.js
@@ -1,14 +1,19 @@
 /* eslint-env node */
 /* eslint-env jasmine */
 
+const path = require('path');
 require('jasmine-enzyme/lib/jest');
 const jasmineReporters = require('jasmine-reporters');
+
+let savePath = 'output/';
+if (process.env.CIRCLE_TEST_REPORTS) {
+  savePath = path.resolve(process.env.CIRCLE_TEST_REPORTS, 'jest');
+}
 
 jasmine.VERBOSE = true;
 jasmine.getEnv().addReporter(
   new jasmineReporters.JUnitXmlReporter({
-    consolidateAll: true,
-    savePath: 'output/',
-    filePrefix: 'test-results'
+    savePath,
+    consolidateAll: false
   })
 );
